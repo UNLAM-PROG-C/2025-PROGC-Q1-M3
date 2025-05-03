779f408ca6887cfafb497a4c691f16b4398ea4c8
anabelacejas <anabela.cejas15@gmail.com>
2025-05-03 17:53:32 -0300

    create processes using fork

--- TP1-parte1/02_Python/fork-processes.py
+++ TP1-parte1/02_Python/fork-processes.py
@@ -15,3 +15,27 @@ def player(id):
         sys.stdout.write(f"Player {id} - Throw {i + 1}: {dice}\n")
         time.sleep(random.uniform(0.1, 0.3))
     sys.stdout.write(f"Player {id} finished with {points} points.\n")
+
+
+def main():
+    processes = []
+
+    for player_id in range(PLAYER):
+        pid = os.fork()
+
+        if pid < 0:
+            sys.exit(f"Error while creating process n° {player_id}")
+        if pid:
+            processes.append(pid)
+        else:
+            player(player_id)
+            os._exit(os.EX_OK)
+
+    for pid in processes:
+        os.waitpid(pid, 0)
+
+    print("All players have finished")
+
+
+if __name__ == "__main__":
+    main()
